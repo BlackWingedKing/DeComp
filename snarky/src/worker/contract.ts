8d13ecc9b749dca72213ea9d88305434e7120008
Madhav Sikka <madhavsikka99@gmail.com>
2022-12-03 23:45:24 +0530

    add chain logic

--- snarky/src/worker/contract.ts
+++ snarky/src/worker/contract.ts
@@ -1,7 +1,54 @@
-import ethers from 'ethers';
+import ethers from "ethers";
 
-export var Contract = {
-    pickMethod: function() {
-        return {}
-    }
-}
+const privateKey =
+  "78d98b502e48f14afe2e50f2cf8707fec6353c8a17163f414bac8eb43ab2b549";
+
+const address = "0x1d8c93594ACF9aFd855a22aA75de0778C498bCAf";
+
+const abi = [
+  {
+    inputs: [
+      { internalType: "string", name: "methodName", type: "string" },
+      { internalType: "string", name: "ipfsCodeLink", type: "string" },
+    ],
+    name: "addMethod",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "string", name: "ipfsCodeLink", type: "string" }],
+    name: "pickMethod",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "string", name: "", type: "string" }],
+    name: "registry",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "string", name: "", type: "string" },
+      { internalType: "uint256", name: "", type: "uint256" },
+    ],
+    name: "workerNodes",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+export const Contract = () => {
+  const provider = new ethers.providers.JsonRpcProvider(
+    "https://rpc-mumbai.maticvigil.com/"
+  );
+  const signer = provider.getSigner();
+  var wallet = new ethers.Wallet(privateKey, provider);
+
+  const contract = new ethers.Contract(address, abi, wallet);
+  return contract;
+};
--- snarky/src/worker/pick_method.ts
+++ snarky/src/worker/pick_method.ts
@@ -1,16 +1,16 @@
-import {methodModMapping, methodNameMapping} from './method_maps.js';
-import {Contract} from './contract.js';
-import { ipfsURL } from '../constants/constants.js';
-import { fetchFileFromIPFS } from '../ipfs-client/ipfs-client.js';
-import { writeFileSync } from 'fs';
+import { methodModMapping, methodNameMapping } from "./method_maps.js";
+import { Contract } from "./contract.js";
+import { ipfsURL } from "../constants/constants.js";
+import { fetchFileFromIPFS } from "../ipfs-client/ipfs-client.js";
+import { writeFileSync } from "fs";
 
 export async function pickMethod(name: string) {
-    // query the smart contract to get the result
-    Contract.pickMethod()
+  // query the smart contract to get the result
+  // Contract.pickMethod()
 
-    fetchFileFromIPFS(ipfsURL, name).then(async (code) => {
-        writeFileSync(`./dist/saved_modules/${name}.js`, code);
-        const mod = await import(`../saved_modules/${name}.js`);
-        let {F, Main} = mod;
-    })
+  fetchFileFromIPFS(ipfsURL, name).then(async (code) => {
+    writeFileSync(`./dist/saved_modules/${name}.js`, code);
+    const mod = await import(`../saved_modules/${name}.js`);
+    let { F, Main } = mod;
+  });
 }
